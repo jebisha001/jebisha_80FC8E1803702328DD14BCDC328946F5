b74ca13285d5d5f5ca6293ddab8ffea0641c9d3c
jebisha001 <144596268+jebisha001@users.noreply.github.com>
2023-09-11 12:18:56 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,11 @@
+def isleapyear(year):
+  if(year/4==0 and year (100!=0) or year/400==0):
+    return True
+  else:
+    return False
+year=2020
+if isleapyear(year):
+  print ("2020 is a leap year")
+else:
+  print ("2020 is  not a leap year")
+  
